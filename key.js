e196bf13ca471f19048ed22a8e7c2fa4d0e78c0a
William Harrison <alex@grahamsg.com>
2019-01-19 11:11:16 -0500

    updating homework

---
+++ key.js
@@ -0,0 +1,26 @@
+console.log('this is loaded');
+var SPOTIFY_ID = "74f2127dedc8480684b2e8fede5fe0ae";
+var SPOTIFY_SECRET ="4799ecf6e75647749e60e6bb5040d2cf";
+
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: "74f2127dedc8480684b2e8fede5fe0ae",
+  secret: "4799ecf6e75647749e60e6bb5040d2cf"
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
+
+
+
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
---
+++ liri.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var fs = require("fs");
+var axios = require("axios");
+var liri = process.argv[2];
+var movieName = process.argv[3];
+var spotify = new Spotify(keys.spotify);
+
+switch (liri) {
+    case "spotify-this-song" :
+    spotifyThisSong();
+    break;
+
+    case "movie-This" :
+    movieThis();
+    break
+};
+
+function movieThis() {
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    request(queryUrl, function (response, body) {
+        var movieResponse = JSON.parse(body);
+        var results =
+        "Title: " + movieResponse.Title + "/n" +
+        "Year: " + movieResponse.Year + "/n" +
+        "IMDB Rating: " + movieResponse.Ratings[0].value + "/n" +
+        "Rotten Tomatoes Rating " + movieResponse.Ratings[1].value + "/n" +
+        "Origin Country: " + movieResponse.Country + "/n" +
+        "Language: " + movieResponse.Language + "/n" +
+        "Plot: " + movieResponse.Plot + "/n" +
+        "Actors: " + movieResponse.Actors + "/n"
+   
+   console.log(results);
+    });
+};
